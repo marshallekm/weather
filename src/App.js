32b3befe1df8235fcb1d28183b4fb6a117c5e456
Erika Kawashima <marshallekm@gmail.com>
2022-09-21 21:17:41 +0900

    .

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import './App.css';
 import React, {useEffect, useState} from 'react'
 
 function App() {
+// const [lat, setLat] = useState();
+// const [long, setLong] = useState();
 
-const [lat, setLat] = useState([]);
-const [long, setLong] = useState([]);
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 useEffect(() => {
-  navigator.geolocation.getCurrentPosition(function(position){
-  setLat(position.coords.latitude);
-  setLong(position.coords.longitude);
-  });
-  console.log(lat)
-  console.log(long)
-}, [lat, long]);
+//   navigator.geolocation.getCurrentPosition(function(position){
+//   setLat(position.coords.latitude);
+//   setLong(position.coords.longitude);
+//   });
+// }, [lat, long]);
 
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`)
-            .then((response) => response.json())
-            .then((weatherData) => console.log(weatherData));
+
+
+
+})
 
   return (
     <div className="App">
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,37 @@
+import React, {useState}  from 'react'
+
 export default function SearchBar (){
+  const [search, setSearch] = useState(
+    {
+      city: ""
+    }
+  )
+
+  const formChange = (event) => {
+    const {name, value} = event.target
+    setSearch(prevSearch => ({
+     ...prevSearch,
+     [name]: value
+    }))
+  }
+
+
+  console.log({search})
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(search)
+  }
+
   return (
     <div>
-      <form action='/' method='get'>
+      <form action='/' method='get' onSubmit={handleSubmit}>
         <input
         type="text"
         id="searchbar"
-        name ="search"
-        placeholder= "How's the weather in..."
+        name="city"
+        onChange = {formChange}
+        placeholder= "Search for a city..."
         />
         <button type="submit">
           Search
